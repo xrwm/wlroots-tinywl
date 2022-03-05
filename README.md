7e61a1ef50ff82710a60c7c56b79335557fa91b3
mbund <bundschuhmark@gmail.com>
2022-03-05 16:25:46 -0500

    fix up packaging & format code

--- README.md
+++ README.md
@@ -1,4 +1,39 @@
-# TinyWL
+# wlroots-tinywl
+
+A wayland compositor based on wlroots and tinywl, written in c, packaged with
+nix. This is really just for me to hack on wayland.
+
+## Building wlroots-tinywl
+
+Simply enter the nix development shell for the dependencies and run commands
+as normal.
+```
+nix develop
+make
+```
+
+Or build with nix directly (without entering a dev shell)
+```
+nix build
+```
+
+Or immediately build and run the compositor directly with
+```
+nix run
+```
+
+## NVIDIA graphics card
+Some recent NVIDIA drivers support GBM (required for wlroots). It should be as
+of [experiemental driver 495.46](https://www.nvidia.com/download/driverResults.aspx/184248)
+and [stable driver 510.47.03](https://www.nvidia.com/download/driverResults.aspx/186156).
+
+If you are having some issues with the cursor showing up, try to start the
+compositor like this:
+```
+WLR_NO_HARDWARE_CURSORS=1 ./tinywl -s firefox
+```
+
+# TinyWL's readme
 
 This is the "minimum viable product" Wayland compositor based on wlroots. It
 aims to implement a Wayland compositor in the fewest lines of code possible,
--- flake.nix
+++ flake.nix
@@ -21,25 +21,15 @@
             inherit system;
           };
 
-          systemLibraries = with pkgs; [
-            xorg.libX11
-            xorg.libXinerama
-            xorg.libXcursor
-            xorg.libXrandr
-            xorg.libXi
-            xorg.libXxf86vm
-            xorg.libXext
-          ];
+          nativeBuildInputs = with pkgs; [ pkg-config wayland-scanner ];
 
-          dynamicLibraries = with pkgs; [
-            vulkan-loader
-            wlroots
+          buildInputs = with pkgs; [
             libxkbcommon
-            wayland-scanner
-            wayland-protocols
-            wayland
             pixman
             udev
+            wayland
+            wayland-protocols
+            inputs.nixpkgs-wayland.packages.${system}.wlroots
           ];
 
           packageName = "tinywl";
@@ -47,50 +37,40 @@
         in
         {
 
+          packages.${packageName} = pkgs.stdenv.mkDerivation {
+            pname = packageName;
+            version = "0.15.1";
+            src = ./.;
+
+            nativeBuildInputs = nativeBuildInputs;
+            buildInputs = buildInputs;
+
+            installPhase = ''
+              runHook preInstall
+
+              mkdir -p $out/bin
+              cp tinywl $out/bin
+
+              runHook postInstall
+            '';
+          };
+
           devShells.dev = pkgs.mkShell {
             buildInputs = with pkgs; [
               gnumake
-
-              pkg-config
-              wayland-scanner
-              libxkbcommon
-              pixman
-              udev
-              wayland
-              wayland-protocols
-              inputs.nixpkgs-wayland.packages.${system}.wlroots
-            ]; # ++ systemLibraries ++ dynamicLibraries;
+            ] ++ nativeBuildInputs ++ buildInputs;
 
             shellHook = ''
               [ $STARSHIP_SHELL ] && exec $STARSHIP_SHELL
             '';
-
             # LD_LIBRARY_PATH = pkgs.lib.strings.makeLibraryPath dynamicLibraries;
-
             CURRENT_PROJECT = packageName;
           };
 
+          defaultPackage = self.packages.${system}.${packageName};
           devShell = self.devShells.${system}.dev;
 
         });
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
